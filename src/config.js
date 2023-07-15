b6329959fc865a5357cd287921427d68a8f1c4ab
wkmie <981687031@qq.com>
2023-07-15 10:39:53 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB",
+    "collection_address": "0x5180db8F5c931aaE63c74266b211F580155ecac8",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "1",
+    "block_chain_id": " 1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
     "title": "Crypto Coven",
